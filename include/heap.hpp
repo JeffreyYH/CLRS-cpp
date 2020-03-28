40668e12e9cf24f118ae0430a201b8a6cd42c99a
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2020-03-27 20:38:52 -0400

    update

--- include/heap.hpp
+++ include/heap.hpp
@@ -1,34 +1,23 @@
-#include <utility>
 #include <iostream>
 #include <vector>
 #include "myUtils.hpp"
 
+template <class T>
 class MaxHeap
 {
 private:
     MyUtils myUtils;
 
 public:
-//    explicit MaxHeap(std::vector<T> A_para)
-//    {
-//        A = A_para;
-//    }
-
     int parent_idx(int i) {return i/2;}
-
     int leftChild_idx(int i) {return 2*i;}
-
     int rightChild_idx(int i) {return 2 * i + 1;}
 
-    template <typename T>
     void maxHeapify(std::vector<T> &A, int i);
-
-    template <typename T>
     void buildMaxHeap(std::vector<T> &A);
+    void heapSort(std::vector<T> &A);
 
-//    void heapSort();
-//
-//    // the following implement functions max-priority queue
+//    // member functions for max-priority queue
 //    void heap_maximum();
 //    void heap_extractMax();
 //    void heap_insert();
@@ -38,8 +27,8 @@ public:
 };
 
 //================= member function implementation ==================//
-template <typename T>
-void MaxHeap::maxHeapify(std::vector<T> &A, int i)
+template <class T>
+void MaxHeap<T>::maxHeapify(std::vector<T> &A, int i)
 {
     int l = leftChild_idx(i);
     int r = rightChild_idx(i);
@@ -60,15 +49,24 @@ void MaxHeap::maxHeapify(std::vector<T> &A, int i)
     }
 }
 
-template <typename T>
-void MaxHeap::buildMaxHeap(std::vector<T> &A)
+template <class T>
+void MaxHeap<T>::buildMaxHeap(std::vector<T> &A)
 {
     for (int i=A.size()/2; i>=0; --i)
         maxHeapify(A, i);
 
 }
 
-//void MaxHeap::heapSort();
+template <class T>
+void MaxHeap<T>::heapSort(std::vector<T> &A)
+{
+    buildMaxHeap(A);
+    for (int i=A.size()/2; i>=1; --i)
+    {
+        myUtils.swap(A[0], A[i]);
+        maxHeapify(A, i);
+    }
+}
 
 //void MaxHeap::heap_maximum();
 //void MaxHeap::heap_extractMax();
--- testApp/heap_main.cpp
+++ testApp/heap_main.cpp
@@ -2,13 +2,8 @@
 
 int main()
 {
-//    std::vector<int> A = {16, 14, 10, 8, 7, 9, 3, 2, 4, 1};
-//    MaxHeap maxHeap;
-
-//    MyUtils myUtils;
-//
-//    int a = 3, b = 4;
-//    myUtils.swap(a, b);
+    std::vector<int> A = {16, 14, 10, 8, 7, 9, 3, 2, 4, 1};
+    MaxHeap<int> maxHeap;
 
     return 0;
 }
