2fe841c5b05e4ddbe9d07b6ec5839b9fc8a99aa0
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2020-03-28 01:46:29 -0400

    update

--- testApp/heap_main.cpp
+++ testApp/heap_main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    std::vector<int> A = {16, 14, 10, 8, 7, 9, 3, 2, 4, 1};
+    vector<int> A = {16, 14, 10, 8, 7, 9, 3, 2, 4, 1};
     MaxHeap<int> maxHeap;
     maxHeap.heapSort(A);
     for (auto a:A)
