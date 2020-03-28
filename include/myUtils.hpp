f10283d0b573e4d53c6e583d6d09477196273768
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2020-03-27 20:27:40 -0400

    update

--- include/myUtils.hpp
+++ include/myUtils.hpp
@@ -1,4 +1,4 @@
-// heap.hpp
+// myUtils.hpp
 // if class template or function template is used, then it's NOT possible to separate
 // class declaration (in h) and class implementation(in cpp)
 // thus we choose to use hpp file here
