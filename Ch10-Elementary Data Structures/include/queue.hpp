429c50ad2b1237985a5425cbcee3d5ad7f1b59e3
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2020-08-01 20:13:02 -0400

    update

--- Ch10-Elementary Data Structures/include/queue.hpp
+++ Ch10-Elementary Data Structures/include/queue.hpp
@@ -6,32 +6,55 @@ class Queue
 {
 public:
     int *Q;
-    int head, tail;
-    Queue(int maxSize);       // constructor
-    ~Queue();              // destructor
+    int head, tail, length;
+    Queue(int maxSize, int initHead);     // constructor
+    ~Queue();                            // destructor
     void enqueue(T x);
     T dequeue();
     void printQueue();
 };
 
 template <class T>
-Queue::Queue(int maxSize)
+Queue<T>::Queue(int maxSize, int initHead)
 {
     Q = new T[maxSize];
-    head = 0;
-    tail = 0;
+    length = maxSize;
+    head = initHead;
+    tail = initHead + 1;
 }
 
 template <class T>
-Queue::~Queue()
+Queue<T>::~Queue()
 {
     delete[] Q;
 }
 
 template <class T>
-void Queue::enqueue(T x)
+void Queue<T>::enqueue(T x)
 {
     Q[tail] = x;
-    if
+    if (tail == length)
+        tail = 0;
+    else
+        tail ++;
+}
 
+template <class T>
+T Queue<T>::dequeue()
+{
+    int x = Q[head];
+    if (head == length)
+        head = 0;
+    else
+        head ++;
+    return x;
+}
+
+template  <class T>
+void Queue<T>::printQueue()
+{
+    cout << "elements: ";
+    for (int i=0; i<length; i++)
+        cout << Q[i] << ' ';
+    cout << endl;
 }
---
+++ Ch10-Elementary Data Structures/src/queue_main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "queue.hpp"
+using namespace std;
+
+int main()
+{
+    Queue<int> queueObj(12, 6);
+    queueObj.enqueue(15);
+
+}
