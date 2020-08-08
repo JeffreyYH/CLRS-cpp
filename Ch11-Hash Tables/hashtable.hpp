6544001bece0c3a66e9f7a162165e6e9e813d614
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2020-08-07 23:28:54 -0400

    update

--- Ch11-Hash Tables/hashtable.hpp
+++ Ch11-Hash Tables/hashtable.hpp
@@ -5,5 +5,9 @@ template <class T>
 class HashTable
 {
 public:
-
+    direct_address_search(k);
+    direct_address_insert(x);
+    direct_address_delete(x);
+    hash_insert(k)
+    hash_search(k)
 };
