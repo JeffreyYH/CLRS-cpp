6d0d33dfdeda6f53ea4def9819339683627b5eba
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2020-08-08 20:35:46 -0400

    update

--- Ch11-Hash Tables/hashtable.hpp
+++ Ch11-Hash Tables/hashtable.hpp
@@ -1,13 +1,23 @@
 #include <iostream>
 using namespace std;
 
+template <typename Dtype>
+Struct tableNode
+{
+    Dtype value;
+    int key;
+}
+
 template <class T>
 class HashTable
 {
 public:
-    direct_address_search(k);
-    direct_address_insert(x);
-    direct_address_delete(x);
+    int *table;
+    HashTable(int length) { table = new table[length];};
+    direct_address_search(k) { return table[k]}
+    direct_address_insert(x) { table[k] = x;};
+    direct_address_delete(x) {table};
     hash_insert(k)
     hash_search(k)
 };
+
