5d767bf4c5a35a633937eb5ee8c2a73470a78852
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2020-08-14 17:35:52 -0400

    update

--- Ch12-Binary Search Trees/bst.hpp
+++ Ch12-Binary Search Trees/bst.hpp
@@ -6,14 +6,14 @@ template <class T>
 class BST
 {
 public:
-    treeNode<T>* tree_search(treeNode<T>* root, T k);
-    void iterative_tree_search(treeNode<T>* root, T k);
-    void tree_minimum(treeNode<T>* root);
-    void tree_maximum(treeNode<T>* root);
-    void tree_successor(treeNode<T>* root);
-    void tree_insert(treeNode<T>* root, T z);
-    void transplant(treeNode<T>* root);
-    void tree_delete(treeNode<T>* root, T z);
+    treeNode<T>* tree_search(treeNode<T>* xNode, T k);
+    treeNode<T> * iterative_tree_search(treeNode<T>* xNode, T k);
+    void tree_minimum(treeNode<T>* xNode);
+    void tree_maximum(treeNode<T>* xNode);
+    void tree_successor(treeNode<T>* xNode);
+    void tree_insert(treeNode<T>* xNode, T z);
+    void transplant(treeNode<T>* xNode);
+    void tree_delete(treeNode<T>* xNode, T z);
 };
 
 template <class T>
@@ -34,4 +34,17 @@ treeNode<T>* BST<T>:: tree_search(treeNode<T>* xNode, T k)
         cout << "right of " << xNode->data << endl;
         return tree_search(xNode->right, k);
     }
+}
+
+template <class T>
+treeNode<T> * BST<T>::iterative_tree_search(treeNode<T> *xNode, T k)
+{
+    while ( xNode != nullptr || xNode->data != k)
+    {
+        if (k < xNode->data)
+            xNode = xNode->left;
+        else
+            xNode = xNode->right;
+    }
+    return xNode;
 }
