a8c83301d0f3c28f23557853e1ed2a6abeeb43e3
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2020-08-13 23:06:38 -0400

    update

--- Ch12-Binary Search Trees/bst.hpp
+++ Ch12-Binary Search Trees/bst.hpp
@@ -6,7 +6,7 @@ template <class T>
 class BST
 {
 public:
-    void tree_search(treeNode<T>* root, T k);
+    treeNode<T>* tree_search(treeNode<T>* root, T k);
     void iterative_tree_search(treeNode<T>* root, T k);
     void tree_minimum(treeNode<T>* root);
     void tree_maximum(treeNode<T>* root);
@@ -15,3 +15,23 @@ public:
     void transplant(treeNode<T>* root);
     void tree_delete(treeNode<T>* root, T z);
 };
+
+template <class T>
+treeNode<T>* BST<T>:: tree_search(treeNode<T>* xNode, T k)
+{
+    if (xNode == nullptr || k == xNode->data)
+    {
+        cout << xNode->data <<" found" << endl;
+        return xNode;
+    }
+    if ( k < xNode->data)
+    {
+        cout << "left of " << xNode->data << endl;
+        return tree_search(xNode->left, k);
+    }
+    else
+    {
+        cout << "right of " << xNode->data << endl;
+        return tree_search(xNode->right, k);
+    }
+}
--- Ch12-Binary Search Trees/main_bst.cpp
+++ Ch12-Binary Search Trees/main_bst.cpp
@@ -3,10 +3,16 @@
 
 int main()
 {
-    treeNode<int> * root = new treeNode(6);
-    root->left = new treeNode(5);
-    root->left->left = new treeNode(2);
-    root->left->right = new treeNode(5);
-    root->right = new treeNode(7);
-    root->right->right = new treeNode(8);
+    treeNode<int> * root = new treeNode(15);
+    root->left = new treeNode(6);
+    root->left->left = new treeNode(3);
+    root->left->right = new treeNode(7);
+
+    root->right = new treeNode(18);
+    root->right->left = new treeNode(17);
+    root->right->right = new treeNode(20);
+
+    // search
+    BST<int> bst;
+    bst.tree_search(root, 55);
 }
