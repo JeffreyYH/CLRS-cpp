b08cb8a446ee394b735d548b5ffab64955319d9b
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2020-08-15 01:49:13 -0400

    update

--- Ch12-Binary Search Trees/main_bst.cpp
+++ Ch12-Binary Search Trees/main_bst.cpp
@@ -5,12 +5,19 @@ int main()
 {
     treeNode<int> * root = new treeNode(15);
     root->left = new treeNode(6);
+    root->left->parent = root;
     root->left->left = new treeNode(3);
+    root->left->left->parent = root->left;
     root->left->right = new treeNode(7);
+    root->left->left->parent = root->left;
 
     root->right = new treeNode(18);
+    root->right->parent = root;
     root->right->left = new treeNode(17);
     root->right->right = new treeNode(20);
+
+    root->right->left->parent =root->right;
+    root->right->right->parent = root->right;
 
     // search
     BST<int> bst;
