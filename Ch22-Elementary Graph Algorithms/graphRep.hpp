a6f53bb257e7eafb917f8e653bcd3aad67ff2486
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2020-10-15 22:47:01 -0400

    update

---
+++ Ch22-Elementary Graph Algorithms/graphRep.hpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <list>
+#include <vector>
+
+template <class T>
+class GraphRep
+{
+    public:
+    std::vector<std::vector<T>> construct_adjMat(int size, std::vector<std::vector<int>> edges);
+    std::vector<std::list<T>> construct_adjList(int size, std::vector<std::vector<int>> edges);
+
+};
+
+template <class T>
+std::vector<std::vector<T>> 
+GraphRep<T>::construct_adjMat(int size, std::vector<std::vector<int>> edges)
+{   
+    std::vector<std::vector<T>> adjMat (size, std::vector<T>(size));
+    for (size_t i=0; i<size; ++i)
+    {
+        for (size_t j=0; j<size; ++j)
+            adjMat[i][j] = 0;
+    }
+    for(size_t i=0; i<edges.size(); ++i)
+    {   
+        adjMat[edges[i][0]][edges[i][1]] = 1;       
+    }
+
+    return adjMat;
+}
+
+template <class T>
+std::vector<std::list<T>> 
+GraphRep<T>:: construct_adjList(int size, std::vector<std::vector<int>> edges)
+{
+    std::vector<std::list<T>> adjList(size);
+    for (size_t i=0; i<edges.size(); i++)
+    {
+        adjList[edges[i][0]].push_back(edges[i][1]);
+    }
+
+    return adjList;
+}
---
+++ Ch22-Elementary Graph Algorithms/main_search.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+#include "graphRep.hpp" 
+#include "dfs.hpp"
+#include "bfs.hpp"
+using namespace std;
+
+int main()
+{
+    const int size = 5;
+    vector<vector<int>> edges {
+                                {0,1}, {0,4}, 
+                                {1,0}, {1,2}, {1,3}, {1,4}, 
+                                {2,1}, {2,3}, 
+                                {3,1}, {3,2}, {3,4}, 
+                                {4,0}, {4,1}, {4,3}
+                            };
+
+    // construct adj matrix
+    GraphRep<int> g_rep;
+
+    cout << "Adjacency matrix:" << endl;
+    vector<vector<int>> adjMat = g_rep.construct_adjMat(size, edges);
+    for(size_t i=0; i<adjMat.size(); ++i)
+    {
+        for (size_t j=0; j<adjMat[0].size(); ++j)
+            cout << adjMat[i][j] << ' ';
+        cout << endl;
+    }
+
+    // construct adj list
+    cout << "Adjacency list:" << endl;
+    vector<list<int>> adjList = g_rep.construct_adjList(size, edges);
+    for (size_t i=0; i<adjList.size(); ++i)
+    {
+        cout << i << "->";
+        for (list<int>::iterator it = adjList[i].begin(); it!=adjList[i].end(); it++)
+        {
+            cout << *it << ' ';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
