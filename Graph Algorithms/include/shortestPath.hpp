984279a7073912112236bb772ef5dcd30d60bc27
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2020-10-30 19:53:30 -0400

    update

--- Graph Algorithms/include/shortestPath.hpp
+++ Graph Algorithms/include/shortestPath.hpp
@@ -1,10 +1,14 @@
 #include "graphRep.hpp"
 
 namespace graphAlgo
-{
+{   
+    template <class T>
     class ShortedPath
     {
         public:
+        void Bellman_Ford(std::unordered_map<graphAlgo::nodePtr<T>, std::list<graphAlgo::nodePtr<T>>> adjListObj, 
+                        std::vector<std::vector<T>> weightedEdges,
+                        T s);
 
 
     };
--- Graph Algorithms/src/main_shortestPath.cpp
+++ Graph Algorithms/src/main_shortestPath.cpp
@@ -7,9 +7,21 @@ int main()
 {
     std::vector<int> vertices {0,3,5,9,11};
     std::vector<std::vector<int>> edges {
-                                {0,3,3}, {0,4}, 
-                                {1,4}, {1,3}, {1,2},
-                                {2,3}, 
-                                {3,4}
+                                {0,3}, {0,5}, 
+                                {3,5}, {3,9},
+                                {5,3}, {5,9}, {5,11},
+                                {9,11},
+                                {11,9}, {11,0}
                             };
+    std::vector<int> weights {3, 5, 
+                            2, 6, 
+                            1, 4, 6,
+                            2,
+                            7, 3};
+    std::vector<std::vector<int>> weightedEdges = edges;
+    for (size_t i=0;i<edges.size(); ++i)
+    {
+        weightedEdges[i].push_back(weights[i]);
+    }
+    bool isUndirected = false;
 }
