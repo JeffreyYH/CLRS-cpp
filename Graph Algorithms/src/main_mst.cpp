1dcb8a0a79c3421e184a2164645622f9fd6d3dd7
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2020-11-10 20:20:08 -0500

    update

--- Graph Algorithms/src/main_mst.cpp
+++ Graph Algorithms/src/main_mst.cpp
@@ -5,10 +5,7 @@ int main()
     std::vector<char> vertices {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
     std::vector<std::vector<char>> edges {
                                 {'a','b'}, {'a','h'}, 
-                                {'t','y'}, {'t','x'},
-                                {'y','t'}, {'y','x'}, {'y','z'},
-                                {'x','z'},
-                                {'z','x'}, {'z','s'}
+                                {'b','h'}, {'b','c'},
                             };
     std::vector<float> weights {3, 5, 
                             2, 6, 
