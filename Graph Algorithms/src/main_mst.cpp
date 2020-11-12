7066e4ea17ef150f60f4576230ee132c734c202b
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2020-11-12 13:28:58 -0500

    update

--- Graph Algorithms/src/main_mst.cpp
+++ Graph Algorithms/src/main_mst.cpp
@@ -2,20 +2,24 @@
 
 int main()
 {
-    std::vector<char> vertices {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
-    std::vector<std::vector<char>> edges {
-                                {'a','b'}, {'a','h'}, 
-                                {'b','h'}, {'b','c'},
-                                {'c','i'}, {'c','f'}, {'c','d'},
-                                {'d','f'}, {'d','e'},
-                                {'e','f'}, 
-                                {'f','g'}, 
-                                {'g','i'}, {'g','h'},
-                                {'h','i'},
-                            };
-    std::vector<float> weights {
-
-                            };
+    std::vector<char> 
+    vertices {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
+    std::vector<std::vector<char>> 
+    edges {
+            {'a','b'}, {'a','h'}, 
+            {'b','h'}, {'b','c'},
+            {'c','i'}, {'c','f'}, {'c','d'},
+            {'d','f'}, {'d','e'},
+            {'e','f'}, 
+            {'f','g'}, 
+            {'g','i'}, {'g','h'},
+            {'h','i'},
+        };
+    std::vector<float>
+    weights {4, 8,
+            11, 8,
+            2, 4, 7,
+        };
     bool isUDG = true; 
 
 }
