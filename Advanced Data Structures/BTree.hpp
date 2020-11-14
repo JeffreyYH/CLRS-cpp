ad711acd76b74b8218cb896042e58b15a12315d6
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2020-11-14 17:52:27 -0500

    update

---
+++ Advanced Data Structures/BTree.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+namespace ds    // data structure
+{
+    class BTree
+    {
+        public:
+
+
+    };
+
+}   // namespace ds
--- Graph Algorithms/include/mst.hpp
+++ Graph Algorithms/include/mst.hpp
@@ -15,19 +15,55 @@ namespace ga
     template <class T>
     void MST<T>::Kruskal(ga::graphPtr<T> G)
     {
-        std::list<std::vector<T>> A;
-        std::set<T> S;
+        // A is used to represent MST,
+        // which is a set contains all the edges of MST
+        std::vector<std::vector<T>> A;
+        std::unordered_map<T, std::shared_ptr<std::set<T>>> Sets;
         for (auto v:G->V)
-            S.insert(v);
+        {
+            std::shared_ptr<std::set<T>> S (new std::set<T>);
+            S->insert(v);
+            Sets[v] = S;
+        }
         
         // sort the edges of G:E into nondecreasing order by weight
+        // using lambda feature of C++
         std::sort(G->E_w.begin(), G->E_w.end(), 
                 [](auto &x, auto &y) {
                     return x.second < y.second;
                 });
         for (auto e: G->E_w)
         {
-            
+            T u = e.first[0];
+            T v = e.first[1];
+            // is u and v in the same set, we will not use edge (u,v)
+            // otherwise there will be a circle
+            if (Sets[u]!=Sets[v])
+            {
+                A.push_back(e.first);
+                // merge the sets which have u and v
+                std::shared_ptr<std::set<T>> S_union (new std::set<T>);
+                
+                std::set_union(Sets[u]->begin(), Sets[u]->end(),
+                                Sets[v]->begin(), Sets[v]->end(),
+                                std::inserter(*S_union, S_union->begin()));
+                // all the element in S_union should have the same set pointer
+                for (auto ele: *S_union)
+                    Sets[ele] = S_union;              
+            }
+        }
+
+        // print MST
+        const bool PRINT = 1;
+        if (PRINT)
+        {
+            std::cout << "Edges of MST In processing order, aka increasing order by weights:" << std::endl;
+            for (auto e:A)
+            {
+                for (auto x:e)
+                    std::cout << x << ' ';
+                std::cout << std::endl;
+            }
         }
 
     }
