fb4cdaefa3aa8673a520eee72c5a1cd884a99d2e
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2021-01-17 13:48:31 -0500

    update

---
+++ Foundations/Ch4-Divide-and-Conquer/maximum_subarray.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class MaximumSubarray
+{
+public:
+    static float find_max_crossing_subarray();
+    static float find_max_subarray();
+};
